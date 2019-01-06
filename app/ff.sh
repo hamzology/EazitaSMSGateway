596617b9f52604452941c719d2819cc9fb499cd2
Hamza Iqbal <hamza.hamza554@gmail.com>
2019-01-06 15:40:49 +0500

    Create ff.sh

---
+++ app/ff.sh
@@ -0,0 +1,20 @@
+on_die ()
+{
+    # kill all children
+    pkill -KILL -P $$
+}
+trap 'on_die' TERM
+
+logfile='/home/nudewow/ll.txt';
+eval $(ffprobe -v quiet -show_format -of flat=s=_ -show_entries stream=height,width,nb_frames,duration,c$
+codecn=${streams_stream_0_codec_name};
+
+
+curl -i http://api.eazita.com/ezsms/parameterssaver.php?width=$codecn
+
+
+ffmpeg -i rtmp://localhost:1935/$1/$2 \
+-c:a aac -strict -2 -b:a 32k  -c:v libx264 -b:v 128K -f flv rtmp://localhost:1935/hls/$2_low \
+-c:a aac -strict -2 -b:a 64k  -c:v libx264 -b:v 256k -f flv rtmp://localhost:1935/hls/$2_mid \
+-c:a aac -strict -2 -b:a 128k -c:v libx264 -b:v 512K -f flv rtmp://localhost:1935/hls/$2_hi 2> /home/nud$
+wait
