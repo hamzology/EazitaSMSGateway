bb64f0d52856433cb3aed91d987410dbcad0862f
Hamza Iqbal <hamza.hamza554@gmail.com>
2019-01-14 09:05:18 +0500

    Update vhost.sh

--- app/vhost.sh
+++ app/vhost.sh
@@ -3,24 +3,36 @@
 user=$1
 domain=$2
 
+sitesAvailable='/etc/apache2/sites-available/'
+userDir='/var/www/'
+sitesAvailabledomain=$sitesAvailable$domain.conf
+
 owner=$(who am i | awk '{print $1}')
 userdir="/home/$user"
 userpublicdir="/home/$user/public_html"
 
 
 if [ "$(whoami)" != 'root' ]; then
-	echo $"You have no permission to run $0 as non-root user. Use sudo"
+	echo -e $"You have no permission to run $0 as non-root user. Use sudo"
 		exit 1;
 fi
 
 while [ "$domain" == "" ]
 do
-	echo -e $"Please provide domain. e.g.dev,staging"
-	read domain
+	echo -e $"Please provide domain."
+		exit 1;
 done
 
-mkdir -p $userdir
-mkdir -p $userpublicdir
+if [ -e $sitesAvailabledomain ]; then
+	echo -e $"This domain already exists.\nPlease Try Another one"
+		exit;
+fi
+
+if ! [ -d $userdir ]; then
+	mkdir -p $userdir
+	mkdir -p $userpublicdir
+	chmod 755 $userdir
+fi
 
 chown -R apache: /home/$domain/
 
