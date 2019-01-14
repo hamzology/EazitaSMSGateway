077614efbb5d0ac8b9c2e172cea6220a0d640a8a
Hamza Iqbal <hamza.hamza554@gmail.com>
2019-01-14 08:57:25 +0500

    Update vhost.sh

--- app/vhost.sh
+++ app/vhost.sh
@@ -1,3 +1,27 @@
+#!/bin/bash
 
-domain='example.com'
-sudo mkdir -p /var/www/$domain/public_html
+user=$1
+domain=$2
+
+owner=$(who am i | awk '{print $1}')
+userdir="/home/$user"
+userpublicdir="/home/$user/public_html"
+
+
+if [ "$(whoami)" != 'root' ]; then
+	echo $"You have no permission to run $0 as non-root user. Use sudo"
+		exit 1;
+fi
+
+while [ "$domain" == "" ]
+do
+	echo -e $"Please provide domain. e.g.dev,staging"
+	read domain
+done
+
+mkdir -p $userdir
+mkdir -p $userpublicdir
+
+chown -R apache: /home/$domain/
+
+
