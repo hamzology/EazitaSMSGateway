8a72d05785ffb18b0d2a6bcebd121fb87b68dfbe
Hamza Iqbal <hamza.hamza554@gmail.com>
2019-01-20 15:50:59 +0500

    Create repnginx.sh

---
+++ app/repnginx.sh
@@ -0,0 +1,82 @@
+yum update
+mkdir ~/bcck
+cp -r /etc/nginx/* ~/bcck/
+
+systemctl stop nginx.service
+systemctl disable nginx.service
+
+yum remove nginx
+rm -rf /etc/nginx
+rm -rf /var/log/nginx
+rm -rf /var/cache/nginx/
+rm -rf /usr/lib/systemd/system/nginx.service
+
+
+yum install pcre pcre-devel openssl openssl-devel zlib zlib-devel -y
+
+mkdir ~/working
+cd ~/working
+wget http://nginx.org/download/nginx-1.15.0.tar.gz
+wget https://github.com/arut/nginx-rtmp-module/archive/master.zip
+yum install unzip
+tar -xvf nginx-1.15.0.tar.gz
+unzip master.zip
+
+cd nginx-1.15.0
+
+
+./configure --prefix=/etc/nginx \
+            --sbin-path=/usr/sbin/nginx \
+            --modules-path=/usr/lib64/nginx/modules \
+            --conf-path=/etc/nginx/nginx.conf \
+            --error-log-path=/var/log/nginx/error.log \
+            --pid-path=/var/run/nginx.pid \
+            --lock-path=/var/run/nginx.lock \
+            --user=nginx \
+            --group=nginx \
+            --build=CentOS \
+            --builddir=nginx-1.15.0 \
+            --with-select_module \
+            --with-poll_module \
+            --with-threads \
+            --with-file-aio \
+            --with-http_ssl_module \
+            --with-http_v2_module \
+            --with-http_realip_module \
+            --with-http_addition_module \
+            --with-http_xslt_module=dynamic \
+            --with-http_image_filter_module=dynamic \
+            --with-http_geoip_module=dynamic \
+            --with-http_sub_module \
+            --with-http_dav_module \
+            --with-http_flv_module \
+            --with-http_mp4_module \
+            --with-http_gunzip_module \
+            --with-http_gzip_static_module \
+            --with-http_auth_request_module \
+            --with-http_random_index_module \
+            --with-http_secure_link_module \
+            --with-http_degradation_module \
+            --with-http_slice_module \
+            --with-http_stub_status_module \
+            --http-log-path=/var/log/nginx/access.log \
+            --http-client-body-temp-path=/var/cache/nginx/client_temp \
+            --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+            --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+            --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+            --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+            --with-mail=dynamic \
+            --with-mail_ssl_module \
+            --with-stream=dynamic \
+            --with-stream_ssl_module \
+            --with-stream_realip_module \
+            --with-stream_geoip_module=dynamic \
+            --with-stream_ssl_preread_module \
+            --with-compat \
+            --with-pcre=../pcre-8.42 \
+            --with-pcre-jit \
+            --with-zlib=../zlib-1.2.11 \
+            --with-openssl=../openssl-1.1.0h \
+            --with-openssl-opt=no-nextprotoneg \
+            --add-module=../nginx-rtmp-module \
+            --with-debug
